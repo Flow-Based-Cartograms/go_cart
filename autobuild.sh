c07f84779b9d5f24ac11f8f4abeb4850a693e08a
Pratyush More <pratyushmore@207-184.priv23.nus.edu.sg>
2017-07-15 14:12:42 +0800

    Added brew link and changed readme

--- autobuild.sh
+++ autobuild.sh
@@ -1,10 +1,10 @@
 # Checking for Operating System
 if [ "$(uname)" == "Darwin" ]; then
     # Install dependencies and Generate Makefile on Mac
-    bash scripts/prepare_env_mac.sh
+    chmod a+x scripts/prepare_env_mac.sh && scripts/prepare_env_mac.sh
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     # Install dependencies and Generate Makefile on Linux
-    bash scripts/prepare_env_linux.sh
+    chmod a+x scripts/prepare_env_linux.sh && scripts/prepare_env_linux.sh
 #elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
     # Do something under 32 bits Windows NT platform
 #elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
