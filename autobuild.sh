ef7ada66485e9f70ae5cb2be6cb166f454459267
Pratyush More <pratyushmore@207-184.priv23.nus.edu.sg>
2017-07-15 15:47:17 +0800

    Changed readme to make instructions on troubleshooting more explicit

--- autobuild.sh
+++ autobuild.sh
@@ -16,12 +16,12 @@ cd cartogram_generator
 
 # Building cartogram generator and placing in root directory
 make clean &>/dev/null
-echo "Cleaned up directory." 
+echo "Cleaned up directory."
 make && \
 cp cartogram ../ && \
 echo "Successfully built cartogram generator." && \
 echo "Run using: ./cartogram [.gen file] [.dat file]." || \
-echo "An error occured. Please refer to troubleshooting instructions."
+echo "An error occured. Please refer to the relevant troubleshooting instructions."
 
 # Moving back to root directory
 cd ..
