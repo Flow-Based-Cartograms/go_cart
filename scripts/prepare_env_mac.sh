aa816e37d6d05cf1f8ecbaf15be2352d2ccad076
Pratyush More <pratyushmore96@gmail.com>
2018-01-22 16:14:18 +0800

    Changed autobuild scripts

--- scripts/prepare_env_mac.sh
+++ scripts/prepare_env_mac.sh
@@ -8,7 +8,7 @@ echo "CC = gcc-$gcc_version
 CFLAGS = -O3 -fopenmp -Wall
 DEPS = cartogram.h
 LIBS = -lfftw3 -lm
-OBJ = main.o cartogram.o integrate.o read_gen.o ps_figure.o fill_with_density.o
+OBJ = main.o cartogram.o ffb_integrate.o diff_integrate.o read_gen.o ps_figure.o fill_with_density.o
 
 %.o: %.c \$(DEPS)
 	\$(CC) -c -o \$@ \$< \$(CFLAGS)
