58177b577ca94b006368eb2eee420668f9480808
Ian Duncan <janskykd@gmail.com>
2019-09-08 15:44:24 +0800

    cleaned up comments in autobuild

--- autobuild.sh
+++ autobuild.sh
@@ -5,7 +5,7 @@ if [ "$(uname)" = "Darwin" ]; then
     # Install dependencies and Generate Makefile on Mac
     ./scripts/build_mac.sh
 elif [ "$(expr substr $(uname -s) 1 5)" = "Linux" ]; then
-    # Install dependencies and Generate Makefile on Linux\
+    # Install dependencies and Generate Makefile on Linux
     ./scripts/build_linux.sh
 else
     echo "Warning: Your environment is currently not supported by autobuild."
